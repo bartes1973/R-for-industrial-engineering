00f30213c9af9bf16dee4cedb59ad6c4922bec6a
Roberto Salazar <56412788+rsalaza4@users.noreply.github.com>
2019-10-10 16:32:12 -0400

    Add files via upload

---
+++ xbar and S charts.R
@@ -0,0 +1,78 @@
+### X-BAR & S-CHARTS ###
+
+# The x-bar and the S-charts are used for continuous data where the sample size (n) is > 10
+
+# Considerations:
+
+# 1. The code only accepts .csv files with just one column with numerical data corresponding to the mesurement
+# 2. The column should not have a header, the code will assign "Measurement" as the column header
+# 3. Review you have installed the qcc package. Run the follwing code on the console: install.packages("qcc")
+
+# Import the qcc library
+library(qcc)
+
+# Read the csv file and name it as df
+df <- read.csv(file.choose(),header = FALSE)
+
+# Assign the column name
+colnames(df) <- c("Measurement")
+
+# Let the user know how many data points are in the data frame
+print(paste("The total number of measurements in the dataset is: ",length(df[,1])))
+
+# Specify the sample size
+sample_size <- as.integer(readline(prompt = "Type in the sample size n (must divide the total number of data points and be greater than 10): "))
+
+# Validate the sample size (must divide the total number of data points and be greater than 10)
+while((length(df[,1])%%sample_size != 0) | (sample_size < 11)){
+  sample_size <- as.integer(readline(prompt = paste("Error! Type in an integer number greater than 10 that divides",length(df[,1]),": ")))
+}
+
+# Specify the total number of samples
+number_of_samples <- length(df[,1])/sample_size
+
+# Assign the data points to the corresponding sample
+Sample <- rep(1:number_of_samples,each=sample_size)
+
+# Add the new column to the existing data frame
+df <- data.frame(df,Sample)
+
+# Group de data points
+measure <- with(df, qcc.groups(Measurement,Sample))
+
+# View the grouped data
+head(measure)
+
+# Specify the measure unit
+measure_unit <- readline(prompt = "Type in the measure unit: ") 
+
+# Create the x-bar chart
+xbar <- qcc(measure, type = "xbar", data.name = measure_unit)
+
+# Specify the warning limits (2 sigmas)
+(warn.limits = limits.xbar(xbar$center, xbar$std.dev, xbar$sizes,2))
+
+# Plot the x-bar chart
+plot(xbar, restore.par = FALSE)
+
+# Plot the warning limit lines
+abline(h = warn.limits, lty = 2, col = "red")
+
+# Create the S-chart
+s_chart <- qcc(measure, type = "S", data.name = measure_unit)
+
+# Get both charts summaries
+summary(xbar)
+summary(s_chart)
+
+# Specify the lower control limit
+LSL <- as.numeric(as.character(readline(prompt = "Type in the lower specification limit: ")))
+
+# Specify the upper control limit
+USL <- as.numeric(as.character(readline(prompt = "Type in the upper specification limit: ")))
+
+# Specify the target
+Target <- as.numeric(as.character(readline(prompt = "Type in the desired target: ")))
+
+# Plot the process capability chart
+process.capability(xbar, spec.limits = c(LSL,USL), target = Target)

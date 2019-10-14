ec59edf018f294363da0d34a09cc5491d551437c
Roberto Salazar <56412788+rsalaza4@users.noreply.github.com>
2019-10-14 10:23:46 -0400

    Add files via upload

---
+++ Cause-and-effect Diagram.R
@@ -0,0 +1,28 @@
+### CAUSE-AND-EFFECT DIAGRAM ###
+
+# Import the SixSigma package
+library(SixSigma)
+
+# Specify the effect to be analyzed
+b.effect <- ""
+
+# Create a vector with the names of the causes classification groups
+b.groups <- c("Measurement","Material","Machine","Monther Nature","Man Power","Method")
+
+# Create a vector that contains the causes
+b.causes <- c(vector(mode = "list",length = length(b.groups)))
+
+# Create lists corresponding to the causes for each corresponding group
+b.causes[1] <- list(c("","",""))
+b.causes[2] <- list(c("","",""))
+b.causes[3] <- list(c("","",""))
+b.causes[4] <- list(c("","",""))
+b.causes[5] <- list(c("","",""))
+b.causes[6] <- list(c("","",""))
+
+# Create the cause-and-effect diagram
+ss.ceDiag(b.effect,
+          b.groups,
+          b.causes,
+          main = "Cause-and-effect diagram",
+          sub = "")
